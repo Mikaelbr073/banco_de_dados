9625425ad86fea2be4e2162c3bd0ce260906b5cf
JJunio <junio.araujo01@yahoo.com>
2022-04-15 20:46:17 -0300

    Mapeamento da classe cliente

--- src/main/java/model/entity/Carteira.java
+++ src/main/java/model/entity/Carteira.java
@@ -1,5 +1,68 @@
 package model.entity;
 
-public class Carteira {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CARTEIRA")
+public class Carteira implements Serializable {
+
+	private static final long serialVersionUID = 4312826459536005744L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_CARTEIRA")
+	private long id;
+
+	@Column(name = "SALDO", nullable = false)
+	private Double saldo = 0.0;
+
+	@Column(name = "PONTOS", nullable = false)
+	private int pontos = 0;
+
+	@OneToOne
+	private Cliente cliente;
+
+	public Carteira() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public int getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(int pontos) {
+		this.pontos = pontos;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 
 }
--- src/main/java/model/entity/Cliente.java
+++ src/main/java/model/entity/Cliente.java
@@ -7,23 +7,52 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "CLIENTE")
 public class Cliente implements Serializable {
-	
+
 	private static final long serialVersionUID = 3387642521978418140L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID_CLIENTE")
 	private Long id;
-	
-	@Column(name = "NOME", nullable = false, length = 45)
+
+	@Column(name = "NOME", nullable = false, length = 255)
 	private String nome;
-	
-	public Cliente() {}
+
+	@Column(name = "END_CEP", length = 8)
+	private String endCep;
+
+	@Column(name = "END_RUA", length = 255)
+	private String endRua;
+
+	@Column(name = "END_CIDADE", length = 255)
+	private String endCidade;
+
+	@Column(name = "RG", length = 50)
+	private String rg;
+
+	@Column(name = "CPF", length = 11)
+	private String cpf;
+
+	@Column(name = "CNPJ", length = 50)
+	private String cnpj;
+
+	@Column(name = "INSCRICAO_ESTADUAL", length = 50)
+	private String inscricaoEstadual;
+
+	@Column(name = "TIPO", length = 20)
+	private String tipo;
+
+	@OneToOne
+	private Carteira carteira;
+
+	public Cliente() {
+	}
 
 	public Long getId() {
 		return id;
@@ -39,6 +68,78 @@ public class Cliente implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getEndCep() {
+		return endCep;
+	}
+
+	public void setEndCep(String endCep) {
+		this.endCep = endCep;
+	}
+
+	public String getEndRua() {
+		return endRua;
+	}
+
+	public void setEndRua(String endRua) {
+		this.endRua = endRua;
+	}
+
+	public String getEndCidade() {
+		return endCidade;
+	}
+
+	public void setEndCidade(String endCidade) {
+		this.endCidade = endCidade;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInscricaoEstadual() {
+		return inscricaoEstadual;
+	}
+
+	public void setInscricaoEstadual(String inscricaoEstadual) {
+		this.inscricaoEstadual = inscricaoEstadual;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Carteira getCarteira() {
+		return carteira;
+	}
+
+	public void setCarteira(Carteira carteira) {
+		this.carteira = carteira;
 	}
 
 	@Override
