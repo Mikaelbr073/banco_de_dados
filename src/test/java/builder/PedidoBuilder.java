8467679bf84c6bab6f4180c779371cebd4d949cb
Mikaelbr073 <mcb3@discente.ifpe.edu.br>
2022-04-17 18:54:32 -0300

    criando pedido builder

---
+++ src/test/java/builder/PedidoBuilder.java
@@ -0,0 +1,54 @@
+package builder;
+
+import java.time.LocalDate;
+
+import model.entity.Cliente;
+import model.entity.Endereco;
+import model.entity.Pedido;
+import model.entity.Produto;
+
+/**
+ * @author Mikaelbr073
+ *
+ */
+public class PedidoBuilder {
+	
+	private Cliente cliente;
+	private double valorTotal;
+	private LocalDate data;
+	private Endereco endereco;
+	
+	public PedidoBuilder() {}
+	
+	public static PedidoBuilder umPedido() {
+		return new PedidoBuilder();
+	}
+	
+	public PedidoBuilder comEnderecoData(Endereco endereco, LocalDate data) {
+		this.endereco = endereco;
+		this.data = data;
+		return this;
+		
+	}
+	public PedidoBuilder completo(Endereco endereco, LocalDate data, Cliente cliente, double valorTotal) {
+		this.endereco = endereco;
+		this.data = data;
+		this.cliente = cliente;
+		this.valorTotal = valorTotal;
+		return this;
+		
+	}
+	
+	public Pedido build() {
+		Pedido pedido = new Pedido();
+		pedido.setCliente(cliente);
+		pedido.setData(data);
+		pedido.setValorTotal(valorTotal);
+		pedido.setEndereco(endereco);
+		return pedido;
+	}
+	
+
+	
+
+}
