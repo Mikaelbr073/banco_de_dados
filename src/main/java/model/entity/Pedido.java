d3b5cdd35cff93e640bfbfc1b40394d10732367c
Mikaelbr073 <mcb3@discente.ifpe.edu.br>
2022-04-17 13:13:17 -0300

    Mapeamento de fornecedor/produto

--- src/main/java/model/entity/Pedido.java
+++ src/main/java/model/entity/Pedido.java
@@ -3,10 +3,112 @@
  */
 package model.entity;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * @author Mikaelbr073
  *
  */
-public class Pedido {
+@Entity
+@Table(name = "PEDIDO")
+public class Pedido implements Serializable {
+
+
+	private static final long serialVersionUID = -3060421892280179341L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_PEDIDO")
+	private Long id;
+	
+	@Column(name = "VALOR_TOTAL", columnDefinition = "DOUBLE")
+	private double valorTotal;
+	
+	@Column(name = "DATA", nullable = false)
+	private LocalDate data;
+	
+	@Embedded
+	private Endereco endereco;
+	 
+	@JoinColumn(name = "ID_CLIENTE", referencedColumnName = "ID_CLIENTE")
+	@ManyToOne
+	private Cliente cliente;
+	
+	public Pedido() {}
+
+	public Pedido(Long id, double valorTotal, LocalDate data, Endereco endereco, Cliente cliente) {
+		super();
+		this.id = id;
+		this.valorTotal = valorTotal;
+		this.data = data;
+		this.endereco = endereco;
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/model/entity/Produto.java
+++ src/main/java/model/entity/Produto.java
@@ -3,10 +3,96 @@
  */
 package model.entity;
 
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 /**
  * @author Mikaelbr073
  *
  */
-public class Produto {
+
+@Entity
+@Table(name = "PRODUTO")
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_PRODUTO",  columnDefinition = "INTEGER")
+	private Long id;
+
+	@Column(name = "NOME")
+	private String nome;
+
+	@Column(name = "DESCRICAO")
+	private String descricao;
+
+	@Column(name = "VALOR", nullable = false, columnDefinition = "DOUBLE PRECISION")
+	private double valor;
+
+	@JoinTable(name = "PRODUTO_FORNECEDOR", joinColumns = {
+			@JoinColumn(name = "PRODUTO_ID", referencedColumnName = "ID_PRODUTO") }, inverseJoinColumns = {
+					@JoinColumn(name = "FORNECEDOR_ID", referencedColumnName = "ID_FORNECEDOR") })
+	@ManyToMany
+	private Collection<Fornecedor> fornecedorCollection; 
+	
+
+	public Produto() {
+	}
+
+	public Produto(Long id, String nome, String descricao, double valor) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
