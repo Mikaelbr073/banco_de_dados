1df312acba932aedabb91f9470596dac69c70e5b
Mikaelbr073 <mcb3@discente.ifpe.edu.br>
2022-04-17 22:02:24 -0300

    CarteiraDAO criada

--- src/main/java/dao/CarteiraDAO.java
+++ src/main/java/dao/CarteiraDAO.java
@@ -1,8 +1,12 @@
 package dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import model.entity.Carteira;
+import model.entity.Cliente;
 
 /**
  * @author JJunio
@@ -19,6 +23,16 @@ public class CarteiraDAO {
 	public Carteira adiciona(Carteira c) {
 		return dao.adiciona(c);
 	}
+	
+	public Carteira procurarPorCliente(Cliente cliente) {
+		EntityManager em = EMFactory.getInstance().getEntityManager();
+		TypedQuery<Carteira> consulta = em.createQuery("SELECT * FROM carteira WHERE id_cliente = " + cliente.getId(), Carteira.class);
+		List<Carteira> carteira = consulta.getResultList();
+		em.close();
+		return carteira.get(0);
+		
+	}
+	
 
 	public EntityManager getEntityManager() {
 		return this.dao.getEntityManager();
